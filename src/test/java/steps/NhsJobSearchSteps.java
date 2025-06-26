651c15fdf56f4f8a87a15ea3fcdbc48931ce910f
BhumikaVaghasiya06 <bhumikavaghasiya006@gmail.com>
2025-06-26 14:20:05 +0100

    Update NhsJobSearchSteps.java

--- src/test/java/steps/NhsJobSearchSteps.java
+++ src/test/java/steps/NhsJobSearchSteps.java
@@ -14,6 +14,7 @@ import java.time.Duration;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class NhsJobSearchSteps {
